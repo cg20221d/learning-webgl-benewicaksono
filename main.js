a1902eb7a994d0706b171837141e982ec02e42bf
unknown <benewicaksono@gmail.com>
2022-09-14 14:51:29 +0700

    feat: gambar titik

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ function main() {
     var gl = kanvas.getContext("webgl")
 
     // Vertex shader
-    var vertexShaderCode = 
-    "void main() {" + 
-    "}"
+    var vertexShaderCode = `
+    void main() {
+        float x = 0.0;
+        float y = 0.0;
+        gl_PointSize = 10.0;
+        gl_Position = vec4(x, y, 0.0, 1.0);
+    }`
 
     var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER)
     gl.shaderSource(vertexShaderObject, vertexShaderCode)
@@ -13,8 +17,12 @@ function main() {
 
     // Fragment shader
     var fragmentShaderCode = `
-    "void main() {
-
+    precision mediump float;
+    void main() {
+        float r = 0.0;
+        float g = 0.0;
+        float b = 1.0;
+        gl_FragColor = vec4(r, g, b, 1.0);
     }`
     var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER)
     gl.shaderSource(fragmentShaderObject, fragmentShaderCode)
@@ -26,7 +34,9 @@ function main() {
     gl.linkProgram(shaderProgram)
     gl.useProgram(shaderProgram)
 
-    gl.clearColor(0.5, 0.5, 0.5, 1.0)
+    gl.clearColor(1.0, 0.65, 0.0, 1.0)
     //          Merah, Hijau, Biru, Transparansi
     gl.clear(gl.COLOR_BUFFER_BIT)
+
+    gl.drawArrays(gl.POINTS, 0, 1)
 }
