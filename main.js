e30d17df44a56795e70d8c8feff442358615863a
unknown <benewicaksono@gmail.com>
2022-09-14 15:11:01 +0700

    feat: gambar 3 titik

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ function main() {
     var kanvas = document.getElementById("kanvas")
     var gl = kanvas.getContext("webgl")
 
+    var vertices = [
+        0.5, 0.5,
+        0.0, 0.0,
+        -0.5, 0.5
+    ]
+
+    var buffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+
     // Vertex shader
     var vertexShaderCode = `
+    attribute vec2 aPosition;
     void main() {
-        float x = 0.0;
-        float y = 0.0;
+        float x = aPosition.x;
+        float y = aPosition.y;
         gl_PointSize = 10.0;
         gl_Position = vec4(x, y, 0.0, 1.0);
     }`
@@ -34,9 +45,13 @@ function main() {
     gl.linkProgram(shaderProgram)
     gl.useProgram(shaderProgram)
 
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition")
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(aPosition)
+
     gl.clearColor(1.0, 0.65, 0.0, 1.0)
     //          Merah, Hijau, Biru, Transparansi
     gl.clear(gl.COLOR_BUFFER_BIT)
 
-    gl.drawArrays(gl.POINTS, 0, 1)
+    gl.drawArrays(gl.POINTS, 0, 3)
 }
